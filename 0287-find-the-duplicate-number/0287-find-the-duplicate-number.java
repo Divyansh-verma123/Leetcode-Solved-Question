b5d19796c6ed2a6aec72a63e0b89ea956bcf4930
Divyansh Verma <24cse138@ipec.org.in>
2026-07-07 21:49:08 +0530

    Time: 38 ms (12%), Space: 78.9 MB (99.17%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+     //   time limit exceed bta rha hai 
+    //    for(int i=0;i<nums.length-1;i++){
+    //     for(int j=i+1;j<nums.length ;j++){
+    //              if (nums[i]==nums[j]){
+    //              return nums[i] ;
+    //    } 
+
+    // }
+    // }
+    // return -1;}}
+
+               Arrays.sort(nums);
+          for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                return nums[i];
+            } 
+            }
+            return -1;
+            
+    }}
+
+
+
