b859c5cfe51f12ec0f1071fa000aea6bf240a5bb
Divyansh Verma <24cse138@ipec.org.in>
2026-07-13 20:41:55 +0530

    Time: 10 ms (27.83%), Space: 47.4 MB (7.09%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] != nums[i + 1]) {
+                return nums[i];
+            } else {
+                i++;
+            }
+        }
+
+        return nums[nums.length-1];
+
+    }
+}
